d7d241fb9e0fbf50be7abb3ac54f54b362c7212b
Antwan Wimberly <antwan.wimberly@gmail.com>
2016-05-18 01:29:50 -0700

    formatting

--- lib/war.rb
+++ lib/war.rb
@@ -5,9 +5,9 @@ require "war/deck"
 require "war/player"
 require "war/game"
 
-deck = War::Deck.new
-dealer = War::Player.new(deck)
+deck     = War::Deck.new
+dealer   = War::Player.new(deck)
 opponent = War::Player.new
-game = War::Game.new(dealer, opponent)
+game     = War::Game.new(dealer, opponent)
 
 game.start
